1e51120407e91a418df85773e59a617ac3f34d82
coguzalp <oguzalp@altogic.com>
2022-08-18 16:55:36 +0300

    Small changes
    Corrected href links and a typo

--- layouts/Sidebar.jsx
+++ layouts/Sidebar.jsx
@@ -930,26 +930,26 @@ export default function Sidebar({
       )}
       <div>
         <ul
-          className="flex flex-wrap items-center mt-3 text-gray-500"
+          className="flex flex-wrap items-center text-xs mt-3 text-gray-500"
           style={{ listStyle: 'none' }}
         >
           <li className="pr-2 pt-2">
             <Link href="writers">Writers</Link>
           </li>
           <li className="pr-2 pt-2">
-            <Link href="blog">Blogs</Link>
+            <Link href="blog">Blog</Link>
           </li>
           <li className="pr-2 pt-2">
             <Link href="privacy">Privacy</Link>
           </li>
           <li className="pr-2 pt-2">
-            <Link href="privacy">Terms</Link>
+            <Link href="terms">Terms</Link>
           </li>
           <li className="pr-2 pt-2">
-            <Link href="privacy">About</Link>
+            <Link href="about">About</Link>
           </li>
           <li className="pr-2 pt-2">
-            <Link href="privacy">Altogic</Link>
+            <Link href="https://www.altogic.com">Altogic</Link>
           </li>
         </ul>
       </div>
