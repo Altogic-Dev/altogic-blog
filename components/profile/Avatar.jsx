ca647fd34bb6ea88ac420f1f870489433af0c0e7
semrebyrk <107842751+semrebyrk@users.noreply.github.com>
2022-09-02 16:25:21 +0300

    avatar fix

--- components/profile/Avatar.jsx
+++ components/profile/Avatar.jsx
@@ -1,3 +1,3 @@
 export default function Avatar({ src, className, alt = '' }) {
-  return <img className={`rounded-full ${className}`} src={src} alt={alt} />;
+  return <img className={`rounded-full object-cover ${className}`} src={src} alt={alt} />;
 }
