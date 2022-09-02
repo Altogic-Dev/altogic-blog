1a964e691b43b1b6b49872ca1568aae10780e745
semrebyrk <107842751+semrebyrk@users.noreply.github.com>
2022-09-02 15:39:15 +0300

    route fix

--- components/sidebar/PopularTopics.jsx
+++ components/sidebar/PopularTopics.jsx
@@ -53,7 +53,7 @@ export default function PopularTopics({ isRelatedTopics }) {
         {topics?.map((topic) => (
           <a
             key={topic.id ?? topic._id}
-            href={`tag/${topic.name}`}
+            href={`/tag/${topic.name}`}
             className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-full tracking-sm text-slate-700 bg-slate-200 transition ease-in-out duration-200 hover:bg-purple-500 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-500"
           >
             {topic.name}
