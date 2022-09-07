d66b8443e434fec9b7437f6711c49e1e26a7b7a0
enesmalikozer <105283668+enesmalikozer@users.noreply.github.com>
2022-09-07 17:16:20 +0300

    typo fix

--- pages/story/[storySlug].jsx
+++ pages/story/[storySlug].jsx
@@ -727,7 +727,7 @@ export default function BlogDetail() {
                           as={Fragment}
                           enter="transition ease-out duration-100"
                           enterFrom="transform opacity-0 scale-95"
-                          enterTo="transform opacity-100 scocale-100"
+                          enterTo="transform opacity-100 scale-100"
                           leave="transition ease-in duration-75"
                           leaveFrom="transform opacity-100 scale-100"
                           leaveTo="transform opacity-0 scale-95"
