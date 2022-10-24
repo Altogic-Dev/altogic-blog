fea994a08ab0489a005a325830797e1e42848ca6
vuralevren <evren@altogic.com>
2022-10-24 15:25:36 +0300

    build failed fix

--- pages/settings.jsx
+++ pages/settings.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Head from 'next/head';
 import { useSelector } from 'react-redux';
-import MyDetails from '@/components/settings/MyDetails';
 import ChangePassword from '@/components/settings/ChangePassword';
 import MySessions from '@/components/settings/MySessions';
 import MyPlans from '@/components/settings/MyPlans';
@@ -9,6 +8,7 @@ import ChangeEmail from '@/components/settings/ChangeEmail';
 import Layout from '@/layouts/Layout';
 import constants from '@/constants';
 import ChangeProfilePicture from '@/components/settings/ChangeProfilePicture';
+import MyDetails from '@/components/settings/MyDetails';
 import { useRouter } from 'next/router';
 
 export default function Settings() {
