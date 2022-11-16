552012e9d094f6c0c7709a72148122e490c880f2
semrebyrk <107842751+semrebyrk@users.noreply.github.com>
2022-11-16 14:45:48 +0300

    sign in redirect fix

--- utils/altogic.js
+++ utils/altogic.js
@@ -1,12 +1,9 @@
 import { createClient } from 'altogic';
 
 
-const options = {
-  signInRedirect: 'https://altogic-blog-eight.vercel.app/login'
-}
+
 const altogic = createClient(
   process.env.NEXT_PUBLIC_ALTOGIC_ENV_URL,
   process.env.NEXT_PUBLIC_ALTOGIC_CLIENT_KEY,
-  options
 );
 export const { db, auth, storage, endpoint, queue, realtime, cache } = altogic;
